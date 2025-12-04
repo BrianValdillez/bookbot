3683a0722779ddc9d1693b65dadfaa8c1b3d99c4
Brian Valdillez <valdillez@alumni.usc.edu>
2025-12-03 21:37:48 -0500

    Added sorted character dictionary + pretty prints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count,get_character_count
+from stats import get_word_count,get_character_count,get_sorted_char_dictionary
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,9 +7,18 @@ def get_book_text(filepath):
     return ""
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_filepath = "books/frankenstein.txt"
+    book_text = get_book_text(book_filepath)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_filepath}...")
+    print("----------- Word Count ----------")
     print(f"Found {get_word_count(book_text)} total words")
-    print(get_character_count(book_text))
-    #print(get_book_text("books/frankenstein.txt"))
+    print("--------- Character Count -------")
+    
+    sorted_chars = get_sorted_char_dictionary(get_character_count(book_text))
+    for entry in sorted_chars:
+        if entry['char'].isalpha():
+            print(f"{entry['char']}: {entry['num']}")
 
 main()
--- stats.py
+++ stats.py
@@ -10,5 +10,16 @@ def get_character_count(book):
             dictionary[c] += 1
         else:
             dictionary[c] = 1
-               
+
     return dictionary
+
+def sort_on(char_entry):
+    return char_entry["num"]
+
+def get_sorted_char_dictionary(dictionary):
+    sorted_dictionary = []
+    for c in dictionary:
+        sorted_dictionary.append({"char": c, "num": dictionary[c]})
+
+    sorted_dictionary.sort(key=sort_on, reverse=True)
+    return sorted_dictionary
